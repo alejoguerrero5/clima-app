9d72aa7a50f6b3293adc1545232cb4dabdf5016a
alejoguerrero5 <alejoguerrero5@gmail.com>
2023-06-23 08:49:16 -0500

    form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>
-      Formulario
+    <div className='contenedor'>
+      <form>
+        <div className='campo'>
+            <label htmlFor='ciudad'>Ciudad</label>
+            <input 
+                type='text'
+                id='ciudad'
+                name='ciudad'
+            />
+        </div>
+
+        <div className='campo'>
+            <label htmlFor='pais'>Pais</label>
+            <select
+                id='pais'
+                name='pais'
+            >
+                <option value=''> Seleccione un país </option>
+                <option value='US'>Estados Unidos</option>
+                <option value='CR'>Costa Rica</option>
+                <option value='AR'>Argentina</option>
+                <option value='CO'>Colombia</option>
+                <option value='MX'>México</option>
+                <option value='ES'>España</option>
+            </select>
+        </div>
+
+            <input 
+                type='submit'
+                value='Consultar Clima'
+            />
+
+      </form>
     </div>
   )
 }
